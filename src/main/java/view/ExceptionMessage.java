941df763a05efd21b0a1d217f2fae38da0f41420
배정현 <bae1004kin@gmail.com>
2024-04-04 09:13:38 +0900

    refactor : 불필요한 공백 제거

--- src/main/java/view/ExceptionMessage.java
+++ src/main/java/view/ExceptionMessage.java
@@ -2,7 +2,6 @@ package view;
 import static constant.LottoConstants.*;
 
 public class ExceptionMessage {
-
     private static final String NOT_NUMBER_RANGE = "[ERROR] 숫자는 " + NUMBER_LOWER_BOUND + "부터 " + NUMBER_UPPER_BOUND + " 사이의 숫자여야 합니다.";
     private static final String NOT_NUMBER_SIZE = "[ERROR] 당첨 번호는 6개 입력 가능합니다.";
     private static final String INPUT_TYPE_ERROR = "[ERROR] 숫자만 입력해 주세요.";
@@ -16,7 +15,6 @@ public class ExceptionMessage {
     public static void sizeException() {
         System.out.println(NOT_NUMBER_SIZE);
     }
-
 
     public static void typeException() {
         System.out.println(INPUT_TYPE_ERROR);
