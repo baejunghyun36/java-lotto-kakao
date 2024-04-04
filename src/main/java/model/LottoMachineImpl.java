a7ecdb95a1fad606925ca08a2c068ee53f220b12
배정현 <bae1004kin@gmail.com>
2024-04-04 09:20:48 +0900

    refactor : 사용자 로또 번호 생성 로직 수정

--- src/main/java/model/LottoMachineImpl.java
+++ src/main/java/model/LottoMachineImpl.java
@@ -1,29 +1,19 @@
 package model;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import static constant.LottoConstants.*;
 
 public class LottoMachineImpl implements LottoMachine {
-    private Numbers numbers;
+    private final List<Integer> lottoNumbers = IntStream.rangeClosed(NUMBER_LOWER_BOUND, NUMBER_UPPER_BOUND)
+            .boxed()
+            .collect(Collectors.toList());
 
-    public LottoMachineImpl() {
-        numbers = new Numbers(generateNumbers());
-    }
-
-    private List<Integer> generateNumbers() {
-        List<Integer> numbers = new ArrayList<>();
-        for (int i = NUMBER_LOWER_BOUND; i <= NUMBER_UPPER_BOUND; i++) {
-            numbers.add(i);
-        }
-        return numbers;
-    }
-
-    public Numbers extractLottoNumbers() {
-        numbers.shuffle();
-        List<Integer> extractNumbers = new ArrayList<>(numbers.getNumbers().subList(0, NUMBER_COUNT));
-        Collections.sort(extractNumbers);
-        return new Numbers(extractNumbers);
+    @Override
+    public UserLotto extractLottoNumbers () {
+        Collections.shuffle(lottoNumbers);
+        return new UserLotto(lottoNumbers.subList(ZERO, LOTTO_BALLS_COUNT));
     }
 }
