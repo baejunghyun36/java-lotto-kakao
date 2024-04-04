27362e28d9d7d87e54047a234e9201c8917d1651
배정현 <bae1004kin@gmail.com>
2024-04-04 09:14:57 +0900

    refactor : 상수값 추가 및 생성자 위치 변경

--- src/main/java/constant/LottoConstants.java
+++ src/main/java/constant/LottoConstants.java
@@ -1,9 +1,9 @@
 package constant;
 
 public class LottoConstants {
-
-    private LottoConstants() {
-    }
+    public static final int ZERO = 0;
+    public static final int ONE = 1;
+    public static final int LOTTO_BALLS_COUNT = 6;
     public static final int LOTTO_PRICE = 1_000;
     public static final int NUMBER_COUNT = 6;
     public static final int NUMBER_LOWER_BOUND = 1;
@@ -15,4 +15,7 @@ public class LottoConstants {
     public static final String INPUT_PAYMENT_AMOUNT = "구입금액을 입력해 주세요.";
     public static final String INPUT_WINNING_NUMBERS = "지난 주 당첨 번호를 입력해 주세요.";
     public static final String INPUT_BONUS_NUMBER = "보너스 볼을 입력해주세요.";
+
+    private LottoConstants() {
+    }
 }
