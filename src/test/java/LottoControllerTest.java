152e3f8ce1b423c5774ec84a6d66ccebc2cbeee1
배정현 <bae1004kin@gmail.com>
2024-04-08 09:03:18 +0900

    test : 로또 개수 -> validation 체크

--- src/test/java/LottoControllerTest.java
+++ src/test/java/LottoControllerTest.java
@@ -1,23 +1,30 @@
 import controller.LottoController;
-import model.LottoMachineImpl;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
-import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.Test;
+import java.util.ArrayList;
+import java.util.List;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatExceptionOfType;
 
 public class LottoControllerTest {
     LottoController lottoController;
 
+    List<String> input;
+
     @BeforeEach
     void setUp() {
-        lottoController = new LottoController(10, new LottoMachineImpl());
+        input = new ArrayList<>();
+        input.add("1,2,3,4,5,6");
+        lottoController = new LottoController(2, input);
     }
 
-    @ParameterizedTest
-    @ValueSource(ints = {1, 2, 3, 4, 10, 15})
-    void 구입한_로또_용지_개수_테스트(int purchaseAmount) {
-        LottoController lottoGame = new LottoController(purchaseAmount, new LottoMachineImpl());
 
-        assertThat(lottoGame.getUserLottos()).hasSize(purchaseAmount);
+    @Test
+    void 사용자_수동_로또_번호_유효성_익셉션_테스트() {
+        input.add("ak,s,1,2,3,4");
+
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() ->
+                        lottoController = new LottoController(10, input)
+                );
     }
 }
