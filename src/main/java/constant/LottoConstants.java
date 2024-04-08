2f30fcabccacc1c643d9ca06f3cd850af87267cf
배정현 <bae1004kin@gmail.com>
2024-04-08 09:02:18 +0900

    refactor : 수동 로또 상수값 추가

--- src/main/java/constant/LottoConstants.java
+++ src/main/java/constant/LottoConstants.java
@@ -8,13 +8,16 @@ public class LottoConstants {
     public static final int NUMBER_COUNT = 6;
     public static final int NUMBER_LOWER_BOUND = 1;
     public static final int NUMBER_UPPER_BOUND = 45;
-    public static final String AMOUNT_MESSAGE = "개를 구매했습니다.";
     public static final String STATISTICS_MESSAGE = "\n당첨 통계\n---------";
     public static final String RESULT_LIST_MESSAGE = "%d개 일치 (%d원) - %d개\n";
-    public static final String RESULT_REWARD_RATE = "총 수익률은 %f입니다.\n";
+    public static final String RESULT_REWARD_RATE = "총 수익률은 %.2f입니다.\n";
+    public static final String RESULT_LOTTO_AMOUNT = "수동으로 %d장, 자동으로 %d개를 구매했습니다.\n";
     public static final String INPUT_PAYMENT_AMOUNT = "구입금액을 입력해 주세요.";
-    public static final String INPUT_WINNING_NUMBERS = "지난 주 당첨 번호를 입력해 주세요.";
+    public static final String INPUT_WINNING_NUMBERS = "\n지난 주 당첨 번호를 입력해 주세요.";
     public static final String INPUT_BONUS_NUMBER = "보너스 볼을 입력해주세요.";
+    public static final String INPUT_MANUAL_AMOUNT = "\n수동으로 구매할 로또 수를 입력해주세요.";
+    public static final String INPUT_MANUAL_LOTTOS = "\n수동으로 구매할 번호를 입력해 주세요.";
+
 
     private LottoConstants() {
     }
