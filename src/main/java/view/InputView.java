07744ecefdfc463f0942feb7f79cddb85c5ea001
배정현 <bae1004kin@gmail.com>
2024-04-08 09:01:35 +0900

    refactor : 수동 로또 입력 구현

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,6 +1,9 @@
 package view;
 
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import static constant.LottoConstants.*;
 
 public class InputView {
@@ -26,5 +29,21 @@ public class InputView {
     public static String readBonusNumber() {
         System.out.println(INPUT_BONUS_NUMBER);
         return SCANNER.nextLine();
+    }
+
+    public static int readManualPurchaseAmount() {
+        System.out.println(INPUT_MANUAL_AMOUNT);
+        String input = SCANNER.nextLine();
+        int amount = InputValidator.checkTypeNumber(input);
+        InputValidator.checkZeroNumber(amount);
+        return amount;
+    }
+
+    public static List<String> readManualLotto(int manualAmount) {
+        System.out.println(INPUT_MANUAL_LOTTOS);
+
+        return IntStream.range(0, manualAmount)
+                .mapToObj(i -> SCANNER.nextLine())
+                .collect(Collectors.toList());
     }
 }
